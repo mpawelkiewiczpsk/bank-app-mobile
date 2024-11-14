35c8f3f2ab91746dd18f644abe5e99bba59fab0b
kamilb13 <kamil.brzoza13@gmail.com>
2024-11-14 19:15:10 +0100

    modified blik screen

--- src/screens/Blik/index.js
+++ src/screens/Blik/index.js
@@ -1,8 +1,13 @@
-import { useLayoutEffect } from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import { useEffect, useLayoutEffect, useState } from 'react';
+import { View, TouchableOpacity, StyleSheet } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import { Button, Text } from 'react-native-paper';
 
 function BlikScreen({ navigation }) {
+  const [blikNumber, setBlikNumber] = useState(() => generateBlikNumber());
+  const [countdown, setCountdown] = useState(4);
+  const [isCounting, setIsCounting] = useState(true);
+
   useLayoutEffect(() => {
     navigation.setOptions({
       headerLeft: () => (
@@ -15,11 +20,65 @@ function BlikScreen({ navigation }) {
       ),
     });
   }, [navigation]);
+
+  useEffect(() => {
+    if (countdown > 0) {
+      const timer = setInterval(() => {
+        setCountdown((prev) => prev - 1);
+      }, 1000);
+      return () => clearInterval(timer);
+    } else {
+      setIsCounting(!isCounting);
+    }
+  }, [countdown]);
+
+  function generateBlikNumber() {
+    return Array.from({ length: 6 }, () => Math.floor(Math.random() * 10)).join(
+      '',
+    );
+  }
+
+  const resetTimer = () => {
+    setIsCounting(!isCounting);
+    setBlikNumber(generateBlikNumber());
+    setCountdown(4);
+  };
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>BlikScreen</Text>
+    <View style={styles.container}>
+      {!isCounting ? (
+        <View style={styles.container}>
+          <Text style={{ fontSize: 24 }}>Code expired</Text>
+          <Button mode="outlined" onPress={resetTimer}>
+            New code
+          </Button>
+        </View>
+      ) : (
+        <View style={styles.container}>
+          <Text style={{ fontSize: 22 }}>Your code is:</Text>
+          <Text style={{ fontSize: 30, fontWeight: 'bold' }}>{blikNumber}</Text>
+          <Text style={{ fontSize: 20 }}>{countdown}s</Text>
+          <Button
+            icon="content-copy"
+            mode="outlined"
+            onPress={() => console.log(blikNumber)}
+          >
+            Copy blik number
+          </Button>
+        </View>
+      )}
     </View>
   );
 }
 
 export default BlikScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+    gap: 15,
+  },
+});
